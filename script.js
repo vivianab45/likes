91d6d93c89ba5479d387cf34f1b7b9f495675d48
vivianab45 <balleza45@gmail.com>
2023-06-20 18:11:56 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var count=9
+var countElement=document.querySelector("#count");
+
+console.log(countElement)
+
+function add1(){
+    count++
+    countElement.innerText= count+ ' '+ "like(s)";
+    console.log(count);
+}
+
+var count2=12
+var count2Element=document.querySelector("#count2");
+
+console.log(count2Element)
+
+function add2(){
+    count2++
+    count2Element.innerText= count2+ ' '+ "like(s)";
+    console.log(count2);
+}
+
+var count3=9
+var count3Element=document.querySelector("#count3");
+
+console.log(count3Element)
+
+function add3(){
+    count3++
+    count3Element.innerText= count3+ ' '+ "like(s)";
+    console.log(count3);
+}
